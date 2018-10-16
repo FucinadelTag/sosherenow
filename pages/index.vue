fa343760168d6d501c9688b2478d00101b84bfa0
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-10-16 22:50:02 +0200

    Fix titolo

--- pages/index.vue
+++ pages/index.vue
@@ -130,7 +130,7 @@
         <div  class="box columns">
             <div class="column">
                 <p>
-                    <span class="title is-5">Prendi SOS Here Now</span>
+                    <span class="title is-4">Prendi SOS Here Now</span>
                 </p>
                 <img src="https://placehold.it/500x500?text=Immagine del pallone" alt="Subito visibile" />
                 <p>
